a8fb0f65acaa8ef49dd4b23cf423995e78aede7b
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-14 02:49:40 +0300

    update the complete version of adding tuple elements

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -1,8 +1,32 @@
-def add_tuple(tuple_a=(1, 89), tuple_b=(88, 11)):
+def add_tuple(tuple_a=(1, 89, 6,7), tuple_b=(88,9,7 )):
+    
     new = []
-    length = len(tuple_a)
+    empty_tuple = (0,)
+
+    length_a = len(tuple_a)
+    length_b = len(tuple_b)
+    length = length_a
+
+    print(length_b)
+    
+    if length_a > 2:
+        length = 2
+    
+    elif length_b > 2:
+        length = 2
+    
+    if length_a == 1 :
+        tuple_a += empty_tuple
+    
+    elif length_b == 1:
+        tuple_b += empty_tuple
+        
+    print(tuple_b)
+
     for i in range(length):
+       
         new_tuple = (tuple_a[i] + tuple_b[i])
+
         new.append(new_tuple)
     print(tuple(new))
 
