3cfa47a697c216f6a6686e815c0407298c4083a4
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-13 17:26:28 +0300

    update the matrix

--- 6-print_matrix_integer.py
+++ 6-print_matrix_integer.py
@@ -1,7 +1,17 @@
 def print_matrix_integer(matrix=[[1,2,3],[4,5,6],[7,8,9]]):
     length = len(matrix)
     for i in range(length):
-        print(matrix[i], end='')
+        print(matrix[i])
 
 
 print_matrix_integer()
+
+
+# !/usr/bin/python3
+# def print_matrix_integer(matrix=[[]]):
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if (j != 0):
+#                 print(" ", end="")
+#             print("{:d}".format(matrix[i][j]), end="")
+#         print()
