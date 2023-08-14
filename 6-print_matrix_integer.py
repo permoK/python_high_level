a2de44651bdb72e3f1fab724dbdaa8a2bd046b46
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-14 03:06:42 +0300

    update

--- 6-print_matrix_integer.py
+++ 6-print_matrix_integer.py
@@ -1,17 +1,7 @@
-def print_matrix_integer(matrix=[[1,2,3],[4,5,6],[7,8,9]]):
-    length = len(matrix)
-    for i in range(length):
-        print(matrix[i])
-
-
-print_matrix_integer()
-
-
-# !/usr/bin/python3
-# def print_matrix_integer(matrix=[[]]):
-#     for i in range(len(matrix)):
-#         for j in range(len(matrix[i])):
-#             if (j != 0):
-#                 print(" ", end="")
-#             print("{:d}".format(matrix[i][j]), end="")
-#         print()
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if (j != 0):
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+        print()
--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -1,33 +1,29 @@
+#This function adds two tuple elemets
 def add_tuple(tuple_a=(1, 89, 6,7), tuple_b=(88,9,7 )):
-    
+    #initiate an empty list and a tuple whose contents are zero
     new = []
-    empty_tuple = (0,)
-
+    zero_tuple = (0,)
+    #find the length of the tuples
     length_a = len(tuple_a)
     length_b = len(tuple_b)
     length = length_a
-
-    print(length_b)
-    
+    # restrict the number of elements to add in a tuple
     if length_a > 2:
         length = 2
-    
     elif length_b > 2:
         length = 2
-    
+    #add the zero tuple when there is a tuple with less than one element
     if length_a == 1 :
-        tuple_a += empty_tuple
-    
+        tuple_a += zero_tuple
     elif length_b == 1:
-        tuple_b += empty_tuple
-        
-    print(tuple_b)
-
+        tuple_b += zero_tuple
+    #iterate while adding the tuple elements
     for i in range(length):
-       
+       #add the elements
         new_tuple = (tuple_a[i] + tuple_b[i])
-
+        #append the elements in a list
         new.append(new_tuple)
+    #convert and print out the list as a tuple
     print(tuple(new))
-
+# call the function
 add_tuple()
