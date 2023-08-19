f3e7a8a64248f7c84af29cbbc0f2811aa497c6bf
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-19 15:24:54 +0300

    update

---
+++ 0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#this fuction computes the square value of all integers of a matrix
+
+def square_matrix_simple(matrix=[1,2,3]):
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        square = matrix[i] * matrix[i]
+        new_matrix[i] = square
+
+    print(new_matrix, matrix)
+
+
+square_matrix_simple()
