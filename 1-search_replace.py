7f9fe36c7a4bd8c41de94b444fdd0eb8f1853025
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-19 16:41:07 +0300

    update

--- 1-search_replace.py
+++ 1-search_replace.py
@@ -1,3 +1,9 @@
 #function that replaces all occurrences of an element by another in a new list
 def search_replace(my_list, search, replace):
-    
+    value = list(map(lambda a: replace if a == search else a, my_list))
+    print(value)
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+search = 2
+replace = 89
+search_replace(my_list,search,replace)
