760471c0fe4893939d8a42d970e017ec884e5848
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-21 01:41:47 +0300

    update

--- 8-simple_delete.py
+++ 8-simple_delete.py
@@ -1,6 +1,9 @@
-def simple_delete(a_dictionary, key="language"):
-    a_dictionary.pop(key)
-    print(a_dictionary)
+def simple_delete(a_dictionary, key="languae"):
+    # if key not in a_dictionary:
+    #     print(a_dictionary)
+    # else:
+        a_dictionary.pop(key, 'default_value')
+        print(a_dictionary)
 
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
