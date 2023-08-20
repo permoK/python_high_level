e45d3f5464db4a104778bbebcf57d073e7b1114d
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-20 23:58:05 +0300

    add

---
+++ 7-update_dictionary.py
@@ -0,0 +1,10 @@
+def update_dictionary(a_dictionary, key, value):
+    # new_dict = key=value
+    
+    print(a_dictionary)
+    
+    
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+key = "language"
+value = "python"
+update_dictionary(a_dictionary, key, value)
