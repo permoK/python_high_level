2d0e7ecf9117227ae01079da98deedf296fa5b2a
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-21 05:37:38 +0300

    update

--- 10-best_score.py
+++ 10-best_score.py
@@ -4,10 +4,12 @@ def best_score(a_dictionary):
     key = list(a_dictionary.keys())
     score.sort(reverse=True)
 
-    for i, j in a_dictionary.items():
+    for (i, j) in a_dictionary.items():
         if j == score[0]:
             best_key = i
     print(f"Best score: {best_key}")
+
+    
 
 a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
 
