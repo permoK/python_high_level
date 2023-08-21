8245ba44c828e84cfbb4b7d1fadc0020dbaaee4f
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-21 04:58:21 +0300

    update

--- 9-multiply_by_2.py
+++ 9-multiply_by_2.py
@@ -1,10 +1,10 @@
 def multiply_by_2(a_dictionary):
 
     values = (list(a_dictionary.values()))
-    
     keys = (list(a_dictionary.keys()))
     keys.sort()
     length = len(values)
+    
     for i in range(length):
         print(f"{keys[i]}: {a_dictionary[keys[i]]*2}")
 
