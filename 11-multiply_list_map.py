f7486d332b4e587c1031bb0a45ddcfe882fdad1a
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-22 05:40:17 +0300

    update changes

--- 11-multiply_list_map.py
+++ 11-multiply_list_map.py
@@ -1,5 +1,5 @@
 def multiply_list_map(my_list=[1, 2, 3, 4, 6], number=0):
-    new_list = list(map(lambda x: x*4, my_list))
+    new_list = list(map(lambda x: x*number, my_list))
     print(new_list)
 
 
