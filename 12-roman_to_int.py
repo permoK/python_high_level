d902a45decb41c47a2fa32f34de38a2878c47da2
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-22 10:45:32 +0300

    add  roman to int

---
+++ 12-roman_to_int.py
@@ -0,0 +1,29 @@
+def roman_to_int(roman_string):
+    n = []
+    romans = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,       
+        "V": 5,
+        "I": 1,
+        }
+    length = len(roman_string)
+    for i in range(length):
+        current = i
+        next = i+1
+        if next == length:
+            next = i
+            
+        if romans[roman_string[current]] >= romans[roman_string[next]]:
+            n.append(romans[roman_string[current]]*1)
+        elif romans[roman_string[current]] < romans[roman_string[next]]:
+            n.append(romans[roman_string[current]]*-1)
+        
+    roman_number = sum(n)
+    print(roman_number)
+    
+roman_string = 'XCV'
+roman_to_int(roman_string)
+
