8afb8327c3b7ba5dcdcd9cab51c8b74d4c8a7e6f
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-24 00:24:16 +0300

    update

--- 102-complex_delete.py
+++ 102-complex_delete.py
@@ -1,6 +1,8 @@
 def complex_delete(a_dictionary, value):
-
-
+    for i in a_dictionary:
+        if a_dictionary[i] != value:
+            values = (a_dictionary[i])
+            print(f"{i}: {values}")
 
 
 complex_delete(a_dictionary={'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}, value='C')
