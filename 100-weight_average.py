0e69c465dd789bab47fcf3646da87c2917d5349c
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-23 23:48:26 +0300

    update

--- 100-weight_average.py
+++ 100-weight_average.py
@@ -1,12 +1,15 @@
-def weight_average(my_list=[(1, 2), (2, 1), (3, 10), (4, 2)]):
-    total_prod = []
-    div_by = []
-    for i, j in my_list:
-        product = i*j
-        total_prod.append(product)
-        div_by.append(j)
-    average = (sum(total_prod))/ (sum(div_by))
-    print(f"Average: {average:0.2f}")
+def weight_average(my_list=[]):
+    if my_list == []:
+        print(0)
+    else:
+        total_prod = []
+        div_by = []
+        for i, j in my_list:
+            product = i*j
+            total_prod.append(product)
+            div_by.append(j)
+        average = (sum(total_prod))/ (sum(div_by))
+        print(f"Average: {average:0.2f}")
 
 weight_average()
       
---
+++ 101-square_matrix_map.py
@@ -0,0 +1,6 @@
+def square_matrix_map(matrix=[[1, 2, 3],[4, 5, 6],[7, 8, 9]]):
+    
+
+
+
+square_matrix_map()
