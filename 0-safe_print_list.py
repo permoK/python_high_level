1b97ec1f8b1aaf5b2091e9cd0e5feb8d4960db35
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-28 23:55:50 +0300

    update

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,7 +1,10 @@
-def safe_print_list(my_list=[1, 2, 3, 4, 5], x=0):
-    length = len(my_list)
-    for i in range(x):
-        print(i,end="")
+def safe_print_list(my_list=[1, 2, 3, 4, 5], x=9):
+    # length = len(my_list)
+    try:
+        for i in range(x):
+            print(my_list[i],end="")
+    except IndexError:
+        print("  x is greater than the length of the list")
 
 #call the function
 safe_print_list()
