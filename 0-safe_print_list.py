59587be5cfc79652572eaca01ee180ac331d53c9
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-28 23:50:01 +0300

    update

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,2 +1,7 @@
-def safe_print_list(my_list=[], x=0):
-    
+def safe_print_list(my_list=[1, 2, 3, 4, 5], x=0):
+    length = len(my_list)
+    for i in range(x):
+        print(i,end="")
+
+#call the function
+safe_print_list()
