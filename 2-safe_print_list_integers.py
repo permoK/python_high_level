6ffbd6b37c5da5ecd2523d4459d34d8017ac037b
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-08-31 00:45:19 +0300

    update

--- 2-safe_print_list_integers.py
+++ 2-safe_print_list_integers.py
@@ -1,4 +1,12 @@
-def safe_print_list_integers(my_list=[], x=0):
-
-
+def safe_print_list_integers(my_list=[1,2,3,"school",4,5], x=6):
+    for i in range(x):
+       
+        try:
+            if my_list[i] == int(my_list[i]):
+                print(my_list[i],end='')
+        except ValueError:
+            pass
+        except IndexError:
+            print(f" list index out of range")
+    
 safe_print_list_integers()
