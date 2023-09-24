f143012fcdf619e75fd4214ac5b4935f33cca72d
MugumoPerm <perminusk21@gmail.com>
2023-09-25 01:31:34 +0300

    update

--- Data structures__ list, tuples, Dictonaries, sets/18. max_subarray.py
+++ Data structures__ list, tuples, Dictonaries, sets/18. max_subarray.py
@@ -1,4 +1,24 @@
-def max_sum(arr=[1,-2, 3, 4, -1, 2, 1, -5, 4]):
+def max_sum(arr=[-2, 1, -3, 4, -1, 2, 1, -5, 4]):
+    length = len(arr)
 
+    left = 0
+    right = length - 1
+    t = 0
+    s = []
+    print(length)
+
+    for i in range(length):
+        
+        max_sum = sum(arr[left:right+1])
+        t  = max_sum
+        
+        if max_sum > t:
+            s.append(max_sum)
+            right -= i
+        
+        elif max_sum < t:
+            left += i
+        
+        print(s)
 
 max_sum()
